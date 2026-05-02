5a3b446e23e81ef73cd76fd90dc151cee2824092
Dibya-z <dibyajyotibehera043@gmail.com>
2026-05-02 17:51:44 +0530

    setup docker infrastructure and fix frontend routing

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
+        host: true,
         proxy: {
             '/api': {
-                target: 'http://localhost:5001',
+                target: process.env.VITE_PROXY_TARGET || 'http://localhost:5001',
                 changeOrigin: true,
             }
         }
